242832636f9f6af310a0a6142c3cd59954438a32
naomi <naomitkm1@gmail.com>
2019-10-31 09:42:34 -0300

    dfs implementada

--- lab1/pill.c
+++ lab1/pill.c
@@ -25,9 +25,10 @@
 #define N_NODES m
 #define N_ADJ n
 #define WHITE 0
-#define GRAY 2
-#define BLACK 3
+#define GRAY 1
+#define BLACK 2
 #define INITIAL_COLOR -1
+#define STACK_MAX_SIZE 1
 
 /* ESTRUTURAS DE DADOS*/
 typedef struct Node Node;
@@ -37,6 +38,7 @@ struct Node{
     int value;
     struct Node * predecessor;
     char color;
+    char cicle_color;
     Adj *adj_list;
 };
 
@@ -51,12 +53,12 @@ typedef struct Graph{
     int n_nodes;
 } Graph;
 
-typedef struct stack{
+typedef struct Stack{
     int max_size;
     int top;
     int end;
-    Node *stack_nodes;
-} stack;
+    Node **stack_nodes;
+} Stack;
 
 
 void create_adj(Graph* graph,int i, int j){
@@ -99,52 +101,114 @@ void print_graph(Graph *graph){
         printf("Node: %d\n\tAdjList: ",i+1);
         temp = graph->nodes[i].adj_list;
         while(temp != NULL){
-            printf("%d ", temp->node->value);
+            printf("(%d, color %d, cicle_color %d) ", temp->node->value,temp->node->color,temp->node->cicle_color);
             temp = temp->next;
         }
         printf("\n");
     }
 }
 
-int create_stack(stack *s){
-    s->max_size = stack_MAX_SIZE;
-    s->top = q->end = 0;
-    s->stack_nodes = (Node*)malloc(sizeof(Node)*STACK_MAX_SIZE);
+int create_stack(Stack *s){
+    int ret = 0;
+    s->max_size = STACK_MAX_SIZE;
+    s->top = -1;
+    s->stack_nodes = (Node**)malloc(sizeof(Node*)*STACK_MAX_SIZE);
     if(s->stack_nodes == NULL){ret = 1;}
-    else{ret = 0;}
     return ret;
 }
 
-void push(stack *s, Node * node){
-    s->stack_nodes[q->top] = node;
-    s->end = (q->top) + 1;
-    if (s->top <= q.max_size-1){
+void push(Stack *s, Node * node){
+    s->top = (s->top) + 1;
+    s->stack_nodes[s->top] = node;
+    //printf("STACK TOP %d\n", s->top);
+    //printf("Expression %d\n",  (((s->max_size)-sizeof(Node*))/sizeof(Node*)));
+    if (s->top >= (((s->max_size)-sizeof(Node*))/sizeof(Node*))){
         (s->max_size) *= 2;
-        s->stack_nodes = (Node*)realloc(sizeof(Node)*(s->max_size);
+        //printf("NEW MAX SIZE: %d\n", s->max_size);
+        s->stack_nodes = (Node**)realloc(s->stack_nodes,(sizeof(Node*)*(s->max_size)));
+        //fflush(stdout);printf("HERE\n");
     }
+    //printf("STACK TOP %d\n", s->top);
 }
 
-int dfs_visit(Graph *graph, Node *node){
-    int ret;
-    stack q;
-    q = create_stack(&q);
+int empty_stack(Stack * s){
+    return (s->top == -1);
+}
 
-    if(q.stack_nodes != NULL){free(q);}
-
+Node *pop(Stack *s){
+    int old_top = s->top;
+    Node *ret = NULL;
+    if(!empty_stack(s)){
+        //printf("NOT NULL\n");
+        ret = (s->stack_nodes)[old_top];
+        s->top = (s->top)-1;
+    }
+    //printf("NULLLLLLL\n");
     return ret;
 }
 
-int dfs(Graph *graph){
+
+
+int dfs_visit_stack(Graph *graph, Node *node){
     int i;
-    int ret;
-    for (i = 0; i < graph->n_nodes; i++){
-        if(graph->nodes[i].color == WHITE){
-            ret =dfs_visit(graph, &(graph->nodes[i]));
+    int ret = 0;
+    Stack s;
+    Node *u, *v;
+    char next_color = INITIAL_COLOR;
+    Adj *edge = NULL, *old_edge = NULL;
+
+    create_stack(&s);
+
+    /*sair*/
+    push(&s, node);
+
+    while(!empty_stack(&s)){
+        //fflush(stdout);printf("Entered HERE!\n");
+        u = pop(&s);
+        if(u->color == WHITE){
+            u->color = GRAY;
+            if(u->predecessor != NULL){
+                u->cicle_color = ((u->predecessor->color) + 1)%2;
+            }else{
+                u->cicle_color = WHITE;
+            }
+        }
+        edge = u->adj_list;
+        i = 0;
+        while(edge != NULL){
+            v = edge->node;
+            if(v != NULL && v->color == WHITE){
+                v->predecessor = u;
+                v->cicle_color = ((u->cicle_color)+1)%2;
+                push(&s,v);
+            }
+            else if(v != NULL && v->color == GRAY){
+                next_color = ((u->color) + 1)%2;
+                if(v->cicle_color != next_color){
+                    printf("VENENO\n");
+                    return 1;
+                 }
+            }
+            old_edge = edge;
+            edge = edge->next;
+            i = i+1;
         }
     }
     return ret;
 }
 
+
+int dfs_stack(Graph *graph){
+    int ret = 0;
+    int i = 0;
+    for(i = 0; i < graph->n_nodes; i++){
+        if(graph->nodes[i].color == WHITE){
+            ret = dfs_visit_stack(graph, &graph->nodes[i]);
+            if(ret > 0){break;}
+        }
+    }
+    return ret;
+}
 
 int main(void){
     int m=0,n=0;
@@ -152,15 +216,15 @@ int main(void){
     int n_adj=0;
     Graph graph;
     Node *node;
-
+    int ret = -1;
     /* Recebe a entrada do programa*/        
     scanf("%d %d", &m, &n);
-    printf("m = %d, n = %d\n", m, n);
+    //printf("m = %d, n = %d\n", m, n);
 
     /* Inicializa o grafo*/
     graph.nodes = (Node*)malloc(sizeof(Node)*N_NODES);
     if(graph.nodes == NULL){
-        printf("Error, malloc failed!\n");
+        //printf("Error, malloc failed!\n");
         return 1;
     }
     graph.n_nodes = N_NODES;
@@ -169,20 +233,25 @@ int main(void){
         graph.nodes[i].predecessor = NULL;
         graph.nodes[i].adj_list = NULL;
         graph.nodes[i].color = WHITE;
+        graph.nodes[i].cicle_color = INITIAL_COLOR;
     }
 
     /* Cria adjacencias*/
     while(n_adj < N_ADJ){
         scanf("%d %d", &i, &j);
-        printf("i = %d, j = %d\n", i, j);
+        //printf("i = %d, j = %d\n", i, j);
         create_adj(&graph, i-1, j-1);
         n_adj++;
     }
 
-    print_graph(&graph);
 
     /* DFS*/
-    
+    ret = dfs_stack(&graph); 
+    if(ret == 0){
+        printf("TESTAR\n");
+    }
     /* Free nas variaveis*/
+    print_graph(&graph);
+
     return 0;
 }
